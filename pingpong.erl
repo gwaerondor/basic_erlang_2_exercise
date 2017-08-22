4609d30e81bce552cc3f88063162e15688e0746d
Robin Larsson <robin.larsson@ericsson.com>
2017-08-22 11:56:06 +0200

    Updates the pingpong exercise

--- pingpong.erl
+++ pingpong.erl
@@ -1,17 +1,20 @@
 -module(pingpong).
 -export([run/0]).
 
+% For fully qualified calls, the functions must be exported.
+
 run() ->
-    Ping_pid = spawn(fun() -> ping() end),
-    Pong_pid = spawn(fun() -> pong() end),
-    Ping_pid ! {Pong_pid, ping}.
+    Ping_pid = spawn(fun() -> handle_ping() end),
+    Pong_pid = spawn(fun() -> handle_pong() end),
+    Pong_pid ! {Ping_pid, ping}.
     
-ping() ->
+handle_ping() ->
     io:format("Ping...~n"),
     timer:sleep(1000),
     {error, not_implemented}.
 
-pong() ->
+handle_pong() ->
     io:format("Pong...~n"),
     timer:sleep(1000),
     {error, not_implemented}.
+
